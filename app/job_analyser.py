74fd8701796ef051b76022102c889a90b8b22796
Matthew Holman <mholman000@gmail.com>
2025-03-13 17:02:22 +0100

    Update analyser logging

--- app/job_analyser.py
+++ app/job_analyser.py
@@ -3,6 +3,7 @@ import re
 from app.db.db import get_db
 from app.handlers.job import JobHandler
 from app.job_scrapers.utils import create_session
+from app.utils.logging import LoggerFactory, LogLevels
 from config import NEGATIVE_MATCH_KEYWORDS, POSITIVE_MATCH_KEYWORDS
 
 REMOTE_REG_EX_PATTERNS = [
@@ -19,6 +20,8 @@ TRUE_REMOTE_COUNTRIES = [
     "European Economic Area",
 ]
 
+logger = LoggerFactory.get_logger("job scraper", log_level=LogLevels.DEBUG)
+
 with next(get_db()) as db_session:
     job_handler = JobHandler(db_session)
     jobs = job_handler.get_unanalysed()
@@ -28,6 +31,7 @@ with next(get_db()) as db_session:
             remote_country.lower() for remote_country in TRUE_REMOTE_COUNTRIES
         ]:
             job_handler.set_true_remote(job)
+            logger.info(f"Job {job.title} at {job.company_name} with country {job.country} is EU remote.")
             continue
 
     session = create_session(is_tls=False, has_retry=True, delay=15)
@@ -46,6 +50,7 @@ with next(get_db()) as db_session:
             match = re.search(pattern, job_description_text, re.IGNORECASE)
             if match is not None:
                 job_handler.set_true_remote(job)
+                logger.info(f"Job {job.title} at {job.company_name} has match with {pattern} in job description text.")
                 break
 
         for pattern in POSITIVE_MATCH_KEYWORDS:
