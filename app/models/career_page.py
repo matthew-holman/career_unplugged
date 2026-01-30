fcdd8e8849c5b3fc4a9820534c234a7b6cb019f7
Matthew Holman <mholman000@gmail.com>
2026-01-30 08:50:24 +0100

    Update carepage model, move attributes to base

--- app/models/career_page.py
+++ app/models/career_page.py
@@ -9,14 +9,14 @@ class CareerPageBase(SQLModel, table=False):  # type: ignore
     company_name: Optional[str] = Field(default=None)
     url: str = Field(nullable=False, unique=True)
     active: bool = Field(default=True, nullable=False)
+    deactivated_at: datetime | None = Field(default=None)
+    last_status_code: int | None = Field(default=None)
+    last_synced_at: datetime | None = Field(default=None)
 
 
 class CareerPage(CareerPageBase, table=True):  # type: ignore  # type: ignore
     __tablename__ = "career_page"
     id: int = Field(default=None, primary_key=True)
-    deactivated_at: datetime | None = Field(default=None)
-    last_status_code: int | None = Field(default=None)
-    last_synced_at: datetime | None = Field(default=None)
 
 
 class CareerPageCreate(CareerPageBase, table=False):  # type: ignore
